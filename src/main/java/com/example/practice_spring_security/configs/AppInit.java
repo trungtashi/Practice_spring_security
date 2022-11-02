374af6cd577e200eded63e5efce4ec6d84635619
trungtashi99 <trungtashi@gmai.com>
2022-11-02 09:08:42 +0700

    Practice spring security

---
+++ src/main/java/com/example/practice_spring_security/configs/AppInit.java
@@ -0,0 +1,21 @@
+package com.example.practice_spring_security.configs;
+
+import com.example.practice_spring_security.security.SecurityConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SecurityConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/java/com/example/practice_spring_security/security/SecurityInit.java
@@ -0,0 +1,6 @@
+package com.example.practice_spring_security.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
